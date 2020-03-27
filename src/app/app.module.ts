7bc5bd622b88e2ade7c781ef99209dc0ddf20c1d
Gabriel Stellini <gabriel.stellini@ozan.com>
2020-03-27 17:13:15 +0100

    Extra: Add PWA service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import {PwaService} from './pwa.service';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [PwaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
